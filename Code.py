3a98f1501c1f89ce0d2430d9a60863731159b851
Kulbhushan Karn <67707132+Kulbhushankarn@users.noreply.github.com>
2021-07-29 12:02:45 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,10 @@
+#this code is written by kulbhushan
+km = float(input ("Type the number of kilometers:"))
+centimeter = km*100000
+print(km, "km =", centimeter, "centimeter")
+inch = km*39370
+print(km, "km =", inch, "inch")
+foot = km*3280.8
+print(km, "km =", foot, "foot")
+millimeter = km*1e+6
+print(km, "km =", millimeter, "millimeter")
